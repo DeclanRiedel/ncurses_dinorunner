63286123391008cf06769bbea74a94d135fa6127
Declan Riedel <declan.riedel@protonmail.com>
2024-09-12 06:10:47 +0200

    little overhaul

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <cmath>
 #include <array>
+#include <algorithm>
 
 // Constants
 const double PI = 3.14159265358979323846;
@@ -16,8 +17,8 @@ const char STAR = '*';
 const char BUSH = '"';
 const char BUSH_ALT = '*';  // Add this line for the alternative bush shape
 const int MAX_OBSTACLE_HEIGHT = 3;
-const double JUMP_HEIGHT = 5.0;
-const double JUMP_DURATION = 16.0;  // Reduced from 20.0 to make the jump faster
+const double JUMP_HEIGHT = 6.0;  // Increased by 20% from 5.0
+const double JUMP_DURATION = 17.6;  // Increased by 10% from 16.0
 const int MIN_OBSTACLE_DISTANCE = 11;  // Reduced from 32 (about 2/3 reduction)
 const int MAX_OBSTACLE_DISTANCE = 21;  // Reduced from 64 (about 2/3 reduction)
 const int GAME_SPEED = 75;  // Increased from 50 (1/3 slower)
@@ -29,28 +30,29 @@ const std::array<std::vector<std::string>, 3> CACTUS_SHAPES = {{
     {" #", "##", " #"}
 }};
 
-// GameObject struct
+// Use enum class for better type safety
+enum class ObjectType { Dino, Obstacle, Star, Bush };
+
+// Simplify GameObject struct
 struct GameObject {
     int x, y;
     std::vector<std::string> shape;
-    int height;
-    int width;
-    bool isCactus;
+    ObjectType type;
     int color;
 };
 
 // Function prototypes
 bool checkCollision(const GameObject& a, const GameObject& b);
 void setup();
-void createObstacle();
+void createObject(ObjectType type);
 void initializeStars();
-void createBush();
 void updateGame();
 void draw();
+void drawDino();
 
 // Global variables
 int WIDTH, HEIGHT, GROUND;
-std::vector<GameObject> obstacles, stars, bushes;
+std::vector<GameObject> gameObjects;
 GameObject dino;
 int score = 0;
 bool gameOver = false;
@@ -60,10 +62,10 @@ int nextObstacleDistance = 0;
 
 // Collision detection
 bool checkCollision(const GameObject& a, const GameObject& b) {
-    for (int ay = 0; ay < a.height; ay++) {
+    for (int ay = 0; ay < a.shape.size(); ay++) {
         for (int ax = 0; ax < a.shape[ay].length(); ax++) {
             if (a.shape[ay][ax] != ' ') {
-                for (int by = 0; by < b.height; by++) {
+                for (int by = 0; by < b.shape.size(); by++) {
                     for (int bx = 0; bx < b.shape[by].length(); bx++) {
                         if (b.shape[by][bx] != ' ' && a.x + ax == b.x + bx && a.y + ay == b.y + by) {
                             return true;
@@ -95,27 +97,38 @@ void setup() {
     init_pair(3, COLOR_GREEN, COLOR_BLACK);   // Green for bushes
 }
 
-// Create a new obstacle
-void createObstacle() {
-    if (nextObstacleDistance <= 0) {
-        bool isCactus = (rand() % 2 == 0);
-        std::vector<std::string> shape;
-        int height, width;
-        
-        if (isCactus) {
-            shape = CACTUS_SHAPES[rand() % CACTUS_SHAPES.size()];
-            height = shape.size();
-            width = shape[0].length();
-        } else {
-            shape = {std::string(1, STONE)};
-            height = 1;
-            width = 1;
+// Create a new object
+void createObject(ObjectType type) {
+    switch (type) {
+        case ObjectType::Obstacle: {
+            bool isCactus = (rand() % 2 == 0);
+            std::vector<std::string> shape;
+            int height, width;
+            
+            if (isCactus) {
+                shape = CACTUS_SHAPES[rand() % CACTUS_SHAPES.size()];
+                height = shape.size();
+                width = shape[0].length();
+            } else {
+                shape = {std::string(1, STONE)};
+                height = 1;
+                width = 1;
+            }
+            
+            gameObjects.push_back({WIDTH - 1, GROUND - height + 1, shape, ObjectType::Obstacle, isCactus ? 1 : 2});
+            break;
         }
-        
-        obstacles.push_back({WIDTH - 1, GROUND - height + 1, shape, height, width, isCactus, 0});
-        nextObstacleDistance = rand() % (MAX_OBSTACLE_DISTANCE - MIN_OBSTACLE_DISTANCE + 1) + MIN_OBSTACLE_DISTANCE;
-    } else {
-        nextObstacleDistance--;
+        case ObjectType::Bush: {
+            char bushShape = (rand() % 2 == 0) ? BUSH : BUSH_ALT;
+            int color = (rand() % 2 == 0) ? 2 : 3;  // 2 for brown (yellow), 3 for green
+            int yOffset = (rand() % 2) * 2 + 1; // Either 1 or 3 rows below the ground
+            gameObjects.push_back({WIDTH - 1, GROUND + yOffset, {std::string(1, bushShape)}, ObjectType::Bush, color});
+            break;
+        }
+        case ObjectType::Star: {
+            gameObjects.push_back({rand() % WIDTH, rand() % (GROUND - 5), {std::string(1, STAR)}, ObjectType::Star, 0});
+            break;
+        }
     }
 }
 
@@ -123,52 +136,49 @@ void createObstacle() {
 void initializeStars() {
     const int NUM_STARS = WIDTH / 4;
     for (int i = 0; i < NUM_STARS; i++) {
-        stars.push_back({rand() % WIDTH, rand() % (GROUND - 5), {std::string(1, STAR)}, 1, 1, false, 0});
-    }
-}
-
-// Create a new bush
-void createBush() {
-    if (rand() % 15 == 0) {
-        char bushShape = (rand() % 2 == 0) ? BUSH : BUSH_ALT;
-        int color = (rand() % 2 == 0) ? 2 : 3;  // 2 for brown (yellow), 3 for green
-        int yOffset = (rand() % 2) * 2 + 1; // Either 1 or 3 rows below the ground
-        bushes.push_back({WIDTH - 1, GROUND + yOffset, {std::string(1, bushShape)}, 1, 1, false, color});
+        createObject(ObjectType::Star);
     }
 }
 
 // Update game state
 void updateGame() {
-    // Move and remove off-screen obstacles
-    for (auto& obj : obstacles) {
-        obj.x -= 1;  // Changed from 2 to 1 to slow down obstacle movement
+    // Move all objects except the dino
+    for (auto& obj : gameObjects) {
+        if (obj.type != ObjectType::Star && obj.type != ObjectType::Dino) {
+            obj.x -= 1;
+        }
     }
-    obstacles.erase(std::remove_if(obstacles.begin(), obstacles.end(),
-        [](const GameObject& obj) { return obj.x + obj.width < 0; }), obstacles.end());
 
-    // Move and remove off-screen bushes
-    for (auto& bush : bushes) {
-        bush.x -= 1;  // Changed from 2 to 1 to slow down bush movement
-    }
-    bushes.erase(std::remove_if(bushes.begin(), bushes.end(),
-        [](const GameObject& bush) { return bush.x + bush.width < 0; }), bushes.end());
+    // Remove off-screen objects, but not the dino
+    gameObjects.erase(std::remove_if(gameObjects.begin(), gameObjects.end(),
+        [](const GameObject& obj) { 
+            return obj.type != ObjectType::Dino && obj.x + obj.shape[0].length() < 0; 
+        }), gameObjects.end());
 
     // Update jump
     if (isJumping) {
-        jumpProgress += 2.0;  // Increased from 1.5 to 2.0 to make the jump faster
+        jumpProgress += 2.0;
         if (jumpProgress >= JUMP_DURATION) {
             isJumping = false;
             jumpProgress = 0.0;
-            dino.y = GROUND;
-        } else {
-            double jumpOffset = sin(jumpProgress / JUMP_DURATION * PI) * JUMP_HEIGHT;
-            dino.y = GROUND - static_cast<int>(jumpOffset);
+        }
+    }
+
+    // Always update dino's position, whether jumping or not
+    double jumpOffset = isJumping ? sin(jumpProgress / JUMP_DURATION * PI) * JUMP_HEIGHT : 0;
+    dino.y = GROUND - static_cast<int>(jumpOffset);
+
+    // Update dino's position in gameObjects
+    for (auto& obj : gameObjects) {
+        if (obj.type == ObjectType::Dino) {
+            obj.y = dino.y;
+            break;
         }
     }
 
     // Check for collisions
-    for (const auto& obj : obstacles) {
-        if (checkCollision(dino, obj)) {
+    for (const auto& obj : gameObjects) {
+        if (obj.type == ObjectType::Obstacle && checkCollision(dino, obj)) {
             gameOver = true;
             return;
         }
@@ -181,34 +191,28 @@ void updateGame() {
 void draw() {
     clear();
 
-    // Draw stars
-    for (const auto& star : stars) {
-        mvaddch(star.y, star.x, star.shape[0][0]);
-    }
-
-    // Draw ground
+    // Draw ground first
     for (int i = 0; i < WIDTH; i++) {
         mvaddch(GROUND, i, '-');
     }
 
-    // Draw obstacles
-    for (const auto& obj : obstacles) {
-        attron(COLOR_PAIR(obj.isCactus ? 1 : 2));
-        for (int i = 0; i < obj.height; i++) {
-            mvprintw(GROUND - obj.height + 1 + i, obj.x, obj.shape[i].c_str());
+    // Draw all game objects
+    for (const auto& obj : gameObjects) {
+        attron(COLOR_PAIR(obj.color));
+        for (int i = 0; i < obj.shape.size(); i++) {
+            for (int j = 0; j < obj.shape[i].length(); j++) {
+                if (obj.shape[i][j] != ' ') {
+                    mvaddch(obj.y + i, obj.x + j, obj.shape[i][j]);
+                }
+            }
         }
-        attroff(COLOR_PAIR(obj.isCactus ? 1 : 2));
+        attroff(COLOR_PAIR(obj.color));
     }
 
-    // Draw dino
-    mvaddch(dino.y, dino.x, dino.shape[0][0]);
-
-    // Draw bushes
-    for (const auto& bush : bushes) {
-        attron(COLOR_PAIR(bush.color));
-        mvaddch(bush.y, bush.x, bush.shape[0][0]);
-        attroff(COLOR_PAIR(bush.color));
-    }
+    // Draw dino separately to ensure it's always on top
+    attron(COLOR_PAIR(dino.color));
+    mvaddch(dino.y, dino.x, DINO);
+    attroff(COLOR_PAIR(dino.color));
 
     // Draw score
     mvprintw(0, 0, "Score: %d", score);
@@ -228,9 +232,17 @@ const std::vector<std::string> DEFEAT_BANNER = {
     "                                 "
 };
 
+// Optionally, you can add a separate function to draw the dino
+void drawDino() {
+    attron(COLOR_PAIR(dino.color));
+    mvprintw(dino.y, dino.x, "%s", dino.shape[0].c_str());
+    attroff(COLOR_PAIR(dino.color));
+}
+
 int main() {
     setup();
-    dino = {10, GROUND, {std::string(1, DINO)}, 1, 1, false, 0};
+    dino = {10, GROUND, {std::string(1, DINO)}, ObjectType::Dino, 0};
+    gameObjects.push_back(dino);  // Add this line to include dino in gameObjects
     
     initializeStars();
 
@@ -241,8 +253,17 @@ int main() {
             jumpProgress = 0.0;
         }
 
-        createObstacle();
-        createBush();
+        if (nextObstacleDistance <= 0) {
+            createObject(ObjectType::Obstacle);
+            nextObstacleDistance = rand() % (MAX_OBSTACLE_DISTANCE - MIN_OBSTACLE_DISTANCE + 1) + MIN_OBSTACLE_DISTANCE;
+        } else {
+            nextObstacleDistance--;
+        }
+
+        if (rand() % 15 == 0) {
+            createObject(ObjectType::Bush);
+        }
+
         updateGame();
         draw();
     }
@@ -254,7 +275,8 @@ int main() {
     int startY = (HEIGHT - DEFEAT_BANNER.size()) / 2 - 2;
     int startX = (WIDTH - DEFEAT_BANNER[0].length()) / 2;
     for (size_t i = 0; i < DEFEAT_BANNER.size(); ++i) {
-        mvprintw(startY + i, startX, DEFEAT_BANNER[i].c_str());
+        // Fix the mvprintw call
+        mvprintw(startY + i, startX, "%s", DEFEAT_BANNER[i].c_str());
     }
 
     // Display final score
